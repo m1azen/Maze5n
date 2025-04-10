3c4f68b06c878142f8b8e3ce6133bfcde73ea23f
m1azen <mazengalalemam@gmail.com>
2025-04-10 15:44:30 +0200

    Update admain.js

--- admain.js
+++ admain.js
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>صفحة الادمن</title>
+    <title>هذة الصفحة خاصة ب مازن جلال فقط</title>
     <script src="https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js"></script>
     <script src="https://www.gstatic.com/firebasejs/9.6.0/firebase-auth.js"></script>
     <script src="https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js"></script>
